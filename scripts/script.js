97a7e297d9701792010a0150e97fc26bac83005f
raphaelroques <raphaelroques@gmail.com>
2020-10-10 22:34:28 -0400

    top-nav toggle first fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,22 @@
 const portfolio = {};
+// portfolio.topNavButtonClicked = false;
 
-myGame.init = function () {
-
+portfolio.topNavClicked = () => {
+  $(window).on('resize', function (e) {
+    let windowWidth = $(window).width();
+    if (windowWidth < 900) {
+      $('.top-nav-button').on('click', function () {
+        $('.slide-menu').toggle();
+      });
+    } else if (windowWidth >= 900) {
+      $('.slide-menu').show();
+    }
+  });
 };
 
+portfolio.init = function () {
+  portfolio.topNavClicked();
+};
 
 $(document).ready(function () {
   portfolio.init();
