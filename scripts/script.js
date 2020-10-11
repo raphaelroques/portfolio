4d21fdeab89fbdbb5868c63d2222cf342db74d2d
raphaelroques <raphaelroques@gmail.com>
2020-10-11 00:27:59 -0400

    top-nav toggle ok fixed

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,9 @@
 const portfolio = {};
-// portfolio.topNavButtonClicked = false;
 
-portfolio.topNavClicked = () => {
-  $(window).on('resize', function (e) {
-    let windowWidth = $(window).width();
-    if (windowWidth < 900) {
-      $('.top-nav-button').on('click', function () {
-        $('.slide-menu').toggle();
-      });
-    } else if (windowWidth >= 900) {
-      $('.slide-menu').show();
-    }
+portfolio.topNavClicked = function () {
+  $('.top-nav-button').on('click', function () {
+    $(this).toggleClass('active');
+    $('.slide-menu').toggleClass('active');
   });
 };
 
