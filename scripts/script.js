d8e0d0dd66ba489586e88fc7f57a82372bab6349
raphaelroques <raphaelroques@gmail.com>
2020-10-14 17:15:01 -0400

    header mods & js header h2 more conditions

--- scripts/script.js
+++ scripts/script.js
@@ -91,6 +91,17 @@ portfolio.projectContainerHeight = function () {
   });
 };
 
+portfolio.imageContainerHeight = function () {
+  const imageContainerWidth = $('.image-container').width();
+  $('.image-container').css({ height: imageContainerWidth * (4 / 3) + 'px' });
+  $(window).resize(function () {
+    const imageContainerWidth = $('.image-container').width();
+    $('.image-container').css({
+      height: imageContainerWidth * (4 / 3) + 'px',
+    });
+  });
+};
+
 // portfolio.scroll = function () {
 //   $(window).scroll(function () {
 //     $('.animation-test').each(function () {
@@ -112,22 +123,59 @@ portfolio.projectContainerHeight = function () {
 
 portfolio.scroll = function () {
   $(window).scroll(function () {
+    const windowwidth = $(window).width();
+    console.log('width', windowwidth);
     const topOfWindow = $(window).scrollTop();
-    console.log(topOfWindow);
-    if (topOfWindow >= 0) {
-      $('.header-container h1').addClass('tracking-in-contract-bck');
-    }
-    if (topOfWindow > 300) {
-      $('.about-me h2').addClass('slide-in-left');
-    }
-    if (topOfWindow > 1000) {
-      $('.skills h2').addClass('slide-in-right');
-    }
-    if (topOfWindow > 1830) {
-      $('.projects h2').addClass('slide-in-left');
-    }
-    if (topOfWindow > 2850) {
-      $('.contact h2').addClass('slide-in-right');
+    console.log('top', topOfWindow);
+    console.log('----');
+    if (windowwidth > 1230) {
+      if (topOfWindow >= 0) {
+        $('.header-container h1').addClass('tracking-in-contract-bck');
+      }
+      if (topOfWindow > 300) {
+        $('.about-me h2').addClass('slide-in-left');
+      }
+      if (topOfWindow > 1000) {
+        $('.skills h2').addClass('slide-in-right');
+      }
+      if (topOfWindow > 1830) {
+        $('.projects h2').addClass('slide-in-left');
+      }
+      if (topOfWindow > 2850) {
+        $('.contact h2').addClass('slide-in-right');
+      }
+    } else if (windowwidth > 900 && windowwidth < 1230) {
+      if (topOfWindow >= 0) {
+        $('.header-container h1').addClass('tracking-in-contract-bck');
+      }
+      if (topOfWindow > 600) {
+        $('.about-me h2').addClass('slide-in-left');
+      }
+      if (topOfWindow > 1225) {
+        $('.skills h2').addClass('slide-in-right');
+      }
+      if (topOfWindow > 2142) {
+        $('.projects h2').addClass('slide-in-left');
+      }
+      if (topOfWindow > 3600) {
+        $('.contact h2').addClass('slide-in-right');
+      }
+    } else if (windowwidth > 400 && windowwidth < 900) {
+      if (topOfWindow >= 0) {
+        $('.header-container h1').addClass('tracking-in-contract-bck');
+      }
+      if (topOfWindow > 600) {
+        $('.about-me h2').addClass('slide-in-left');
+      }
+      if (topOfWindow > 1225) {
+        $('.skills h2').addClass('slide-in-right');
+      }
+      if (topOfWindow > 2142) {
+        $('.projects h2').addClass('slide-in-left');
+      }
+      if (topOfWindow > 7000) {
+        $('.contact h2').addClass('slide-in-right');
+      }
     }
   });
 };
@@ -136,6 +184,7 @@ portfolio.init = function () {
   portfolio.topNavClicked();
   portfolio.slideMenuCliked();
   portfolio.projectContainerHeight();
+  portfolio.imageContainerHeight();
   portfolio.scroll();
 };
 
